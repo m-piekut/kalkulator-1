90afe037493e6592e23721f920d950e72e6bc056
Marek Piekut <piekut358@gmail.com>
2020-07-08 16:15:13 +0200

    przechodzenie po menu

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@ const add = document.querySelector('.nav-add')
 const popup = document.querySelector('.popup')
 
 
+const homeBtn = document.querySelector('.nav-home')
+const rentBtn = document.querySelector('.nav-rent')
+const clothesBtn = document.querySelector('.nav-clothes')
+const healthBtn = document.querySelector('.nav-health')
+const relaxBtn = document.querySelector('.nav-relax')
+const transportBtn = document.querySelector('.nav-transport')
+const foodBtn = document.querySelector('.nav-food')
+const othersBtn = document.querySelector('.nav-others')
+const allBtn = document.querySelector('.nav-all')
+
+const homeSection = document.querySelector('#home')
+const rentSection = document.querySelector('#rent')
+const clothesSection = document.querySelector('#clothes')
+const healthSection = document.querySelector('#health')
+const relaxSection = document.querySelector('#relax')
+const transportSection = document.querySelector('#transport')
+const foodSection = document.querySelector('#food')
+const othersSection = document.querySelector('#others')
+const allSection = document.querySelector('#all')
+const allSections = [homeSection, rentSection, clothesSection, healthSection, relaxSection, transportSection, foodSection, othersSection, allSection]
 
 
 haburger.addEventListener('click', ()=>{
@@ -23,6 +43,77 @@ add.addEventListener('click', ()=>{
 })
 
 
+const hideSection = ()=>{
+        allSections.forEach((e)=>{
+            e.classList.remove('show-section')
+        })
+    }
+
+
+
+
+
+
+
+
+homeBtn.addEventListener('click', ()=>{
+    hideSection()
+    homeSection.classList.add('show-section')
+})
+rentBtn.addEventListener('click', ()=>{
+    hideSection()
+    rentSection.classList.add('show-section')
+})
+clothesBtn.addEventListener('click', ()=>{
+    hideSection()
+    clothesSection.classList.add('show-section')
+})
+healthBtn.addEventListener('click', ()=>{
+    hideSection()
+    healthSection.classList.add('show-section')
+})
+relaxBtn.addEventListener('click', ()=>{
+    hideSection()
+    relaxSection.classList.add('show-section')
+})
+transportBtn.addEventListener('click', ()=>{
+    hideSection()
+    transportSection.classList.add('show-section')
+})
+foodBtn.addEventListener('click', ()=>{
+    hideSection()
+    foodSection.classList.add('show-section')
+})
+othersBtn.addEventListener('click', ()=>{
+    hideSection()
+    othersSection.classList.add('show-section')
+})
+allBtn.addEventListener('click', ()=>{
+    hideSection()
+    allSection.classList.add('show-section')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// wykres
+
 const ctx = document.getElementById('myChart').getContext('2d');
 const chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -33,16 +124,17 @@ const chart = new Chart(ctx, {
         labels: ['Czynsz, kredyt rachunki', 'Ubrania', 'Zdrowie, chemia, higiena', 'Relaks, hobby, kino', 'Paliwo, bilety, transport', 'Jedzenie', 'Inne'],
         datasets: [{
             label: 'Wszytkie wydatki',
-            backgroundColor: ['rgb(255, 99, 132)',
-                            'rgb(0, 99, 132)',
-                            'rgb(0, 22, 132)',
-                            'rgb(0, 99, 132)',
-                            'rgb(0, 99, 11)',
-                            'rgb(0, 33, 132)',
-                            'rgb(0, 0, 0)'],
+            backgroundColor: [
+                            '#4A5899',
+                            '#ECB42E',
+                            '#F84AA7',
+                            '#5FAD56',
+                            '#CF5C36',
+                            '#FFF275',
+                            '#ABABB0'],
 
 
-            borderColor: 'rgb(255, 99, 132)',
+            borderColor: '#001242',
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
     },
